e18bf22134468a433a51a07f8b04c672b621d869
ricebun <renicegohyc@gmail.com>
2021-02-09 14:41:59 +0800

    heroku

--- src/components/Login.js
+++ src/components/Login.js
@@ -46,7 +46,7 @@ const Login = () => {
         console.log("submitting..", formData)
         // const csrftoken = Cookies.get('csrftoken')
         axios
-            .post('http://localhost/auth/login', formData)
+            .post('/auth/login', formData)
             .then(response => {
                 console.log(response)
                 sessionStorage.setItem('token', response.data.key)
