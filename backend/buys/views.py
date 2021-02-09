77c8f0238b55a80da96535ed99a509ee003336f7
ricebun <renicegohyc@gmail.com>
2021-02-09 12:35:10 +0800

    heroku

--- backend/buys/views.py
+++ backend/buys/views.py
@@ -1,14 +1,15 @@
 from backend.buys import serializers
 from backend.buys.models import Buy, Item, Order, OrderItem
 from backend.buys.serializers import BuySerializer, ItemSerializer, OrderSerializer, OrderItemSerializer
-from rest_framework import generics, permissions, renderers, authentication, View
+from rest_framework import generics, permissions, renderers, authentication
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.http import HttpResponse, HttpResponseNotFound
 
 
-class Assets(View):
+class Assets(viewsets.ViewSet):
     def get(self, _request, filename):
         path = os.path.join(os.path.dirname(__file__), 'static', filename)
         if os.path.isfile(path):
