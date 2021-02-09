65be9c24e20148e853d811e6e0ef52cb954dd425
ricebun <renicegohyc@gmail.com>
2021-02-09 13:05:32 +0800

    heroku

--- backend/settings.py
+++ backend/settings.py
@@ -147,6 +147,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 options = DATABASES['default'].get('OPTIONS', {})
 options.pop('sslmode', None)
 
